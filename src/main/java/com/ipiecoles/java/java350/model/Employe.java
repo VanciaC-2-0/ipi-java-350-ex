604cf30928d6656b4981b2c1a313e61f1e61cbf2
VanciaC <vancia.chan@ipilyon.net>
2021-02-23 13:51:39 +0100

    test

--- src/main/java/com/ipiecoles/java/java350/model/Employe.java
+++ src/main/java/com/ipiecoles/java/java350/model/Employe.java
@@ -255,6 +255,7 @@ public class Employe {
         this.tempsPartiel = tempsPartiel;
     }
 
+    /*
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -273,4 +274,6 @@ public class Employe {
     public int hashCode() {
         return Objects.hash(id, nom, prenom, matricule, dateEmbauche, salaire, performance);
     }
+
+     */
 }
